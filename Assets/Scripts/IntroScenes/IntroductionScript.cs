2d5fad4a05ff6c2514e80d454d447bdfe1ba9f0e
vladonee <vlad.osan13@gmail.com>
2024-03-10 18:24:03 +0200

    V3.5(username din scena 1->3)

--- Assets/Scripts/IntroScenes/IntroductionScript.cs
+++ Assets/Scripts/IntroScenes/IntroductionScript.cs
@@ -2,11 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class IntroductionScript : MonoBehaviour
 {
+    public static IntroductionScript scene1;
+    public TMP_InputField inputField;
+
+    public string player_name;
+
+    private void Awake()
+    {
+        if (scene1 == null)
+        {
+            scene1 = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
     public void Continue()
     {
+        player_name = inputField.text;
+
         SceneManager.LoadScene(2);
     }
 
